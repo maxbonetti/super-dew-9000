04abe87142c1ea8a2fdc7449b1e33b847d641f51
Max Bonetti <maxwell.bonetti@gmail.com>
2024-06-16 00:57:35 -0500

    fixed about me

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
             <h2>About Me</h2>
             <img src="/Images/Picture1.jpg" alt="Professional Photo of Myself" />
             <p>
-                Multi certified and highly motivated individual experienced with over three years of commercial and residential customer service and sales
+                Multi certified and highly motivated individual experienced with over two years of Tech, coding as well as, three years of commercial and residential customer service and sales
                 experience. Seeking to contribute my talent and diversify my experience in the IT industry. Dedicated coach-able team player
                 with strong work ethic. Highly skilled in time management, employee development, team building and interpersonal
                 communications. Recognized for strong leadership and conflict resolution expertise.
