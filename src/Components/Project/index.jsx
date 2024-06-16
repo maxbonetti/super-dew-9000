c42468ebd6ad1009bd97b794cf1b3681ef450267
Max Bonetti <maxwell.bonetti@gmail.com>
2024-06-15 22:35:55 -0500

    styles added to project.index for rendering of pics better. idealy make the container hover the full image.

--- src/Components/Project/index.jsx
+++ src/Components/Project/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../../styles/Project.css';
 
 const Project = ({ title, imgSrc, deployedLink, repoLink }) => {
     return (
-        <div>
+        <div className="project">
             <h3>{title}</h3>
-            <img src={imgSrc} alt={`${title} screenshot`} />
-            <a href={deployedLink}>View App</a>
-            <a href={repoLink}>GitHub Repo</a>
+            <img src={imgSrc} className="project-image" alt={`${title}  screenshot`} />
+           <div className="project-links" >
+            <a href={deployedLink} className="btn btn-primary">View Deployed App</a>
+            <a href={repoLink} className="btn btn-secondary">GitHub Repo</a>
+            </div>
         </div>
     );
 };
